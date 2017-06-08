5e4d24b337be91a4a04fd0dc34fe493a2ec0582e
aexol <aexol@aexol.com>
2017-06-08 15:32:19 +0200

    Edited jwt

--- src/django/index.jsx
+++ src/django/index.jsx
@@ -1,76 +1,76 @@
 import Cookies from 'js-cookie';
 export const getCookie = (name) => {
-	var cookieValue = null;
-	if (document.cookie && document.cookie != '') {
-		var cookies = document.cookie.split(';');
-		for (var i = 0; i < cookies.length; i++) {
-			var cookie = jQuery.trim(cookies[i]);
-			if (cookie.substring(0, name.length + 1) == (name + '=')) {
-				cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-				break;
-			}
-		}
-	}
-	return cookieValue;
+  var cookieValue = null;
+  if (document.cookie && document.cookie != '') {
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++) {
+      var cookie = jQuery.trim(cookies[i]);
+      if (cookie.substring(0, name.length + 1) == (name + '=')) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
+    }
+  }
+  return cookieValue;
 }
 export const djangoFetch = (url,
 params) => {
-	return fetch(url, {
-		method: "get",
-		credentials: "include",
-		headers: {
-			"X-CSRFToken": getCookie("csrftoken"),
-			"Accept": "application/json",
-			"Content-Type": "application/json"
-		},
-		...params
-	})
+  return fetch(url, {
+    method: "get",
+    credentials: "include",
+    headers: {
+      "X-CSRFToken": getCookie("csrftoken"),
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+    },
+    ...params
+  })
 }
 export const djangoJWT = (url,
 params,
 headers = {},
 isProfile = 0) => {
-	const token = Cookies.get("prawowpolsce_t")
-	return fetch(url, {
-		method: "get",
-		credentials: "include",
-		headers: {
-			"Authorization": "JWT " + token,
-			"Accept": "application/json",
-			"Content-Type": "application/json",
-			...headers
-		},
-		...params
-	}).then(response => {
-		if (response.status == 401) {
-			if (!isProfile) {
-				window.location.href = "/accounts/profile/"
-			} else {
-				return response
-			}
-		} else {
-			return response
-		}
-	})
+  const token = Cookies.get("jwt-service-aexol-token")
+  return fetch(url, {
+    method: "get",
+    credentials: "include",
+    headers: {
+      "Authorization": "JWT " + token,
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+      ...headers
+    },
+    ...params
+  }).then(response => {
+    if (response.status == 401) {
+      if (!isProfile) {
+        window.location.href = "/accounts/profile/"
+      } else {
+        return response
+      }
+    } else {
+      return response
+    }
+  })
 }
 export const djangoJWTMulti = (url,
 params,
 headers = {}) => {
-	const token = Cookies.get("prawowpolsce_t")
-	return fetch(url, {
-		method: "get",
-		credentials: "include",
-		headers: {
-			"Authorization": "JWT " + token,
-			"Accept": "application/json",
-			...headers
-		},
-		...params
-	}).then(response => {
-		if (response.status == 401) {
-			window.location.href = "/accounts/profile/"
-		} else {
-			return response
-		}
-	})
+  const token = Cookies.get("jwt-service-aexol-token")
+  return fetch(url, {
+    method: "get",
+    credentials: "include",
+    headers: {
+      "Authorization": "JWT " + token,
+      "Accept": "application/json",
+      ...headers
+    },
+    ...params
+  }).then(response => {
+    if (response.status == 401) {
+      window.location.href = "/accounts/profile/"
+    } else {
+      return response
+    }
+  })
 }

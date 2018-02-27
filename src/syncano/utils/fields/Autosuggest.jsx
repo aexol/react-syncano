bd43d97c615cfb1b2259152a179a1a8b2f647536
Olga Poniatowska <olga@Olgas-Mac-mini.local>
2018-02-27 10:49:51 +0100

    autosuggest

--- src/syncano/utils/fields/Autosuggest.jsx
+++ src/syncano/utils/fields/Autosuggest.jsx
@@ -1,35 +1,38 @@
 import React, { PropTypes } from 'react'
+import classnames from 'classnames'
+import './Autosuggest.scss'
+
 class Autosuggest extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {}
+    this.state = {
+      showDatalist: false,
+      setValue: ''
+    }
   }
   componentWillMount() {
     const { fieldValue='' } = this.props
     this.setState({
-      value: fieldValue
+      setValue: fieldValue
     })
   }
   componentWillReceiveProps(nextProps) {
     if(nextProps.fieldValue && this.state.value === ''){
       this.setState({
-        value:nextProps.fieldValue
+        setValue: nextProps.fieldValue
       })
     }
   }
   onChange = (e) => {
     const { load, onSelect } = this.props
-    const { value } = e.target
-    this.setState({ value })
-    if (value.length > 2) {
-      load(value)
-      onSelect(value)
+    const newValue = e.target.value;
+    this.setState({ setValue: newValue, showDatalist: true })
+    if (newValue.length > 2) {
+      load(newValue)
+      onSelect(newValue)
     }
   }
   render() {
-    const {
-          value = ""
-        } = this.state
     const {
       onSelect,
       list = [],
@@ -40,11 +43,29 @@ class Autosuggest extends React.Component {
       ...props
     } = this.props
     return (
-      <div className="Autosuggest">
-        <input {...props} onChange={this.onChange} value={value} type="text" list={name} placeholder={placeholder || name} />
-        <datalist id={name}>
-          {list && list.map((i, index) => <option key={index} value={i} />)}
-        </datalist>
+      <div className='autosuggest'>
+        <div className="Autosuggest">
+          <input {...props} onChange={this.onChange} value={this.state.setValue} type="text" list={name} placeholder={placeholder || name} />
+          {list &&
+            <div className={classnames({
+              'datalist-suggest': true,
+              'show-datalist': this.state.showDatalist
+            })}>
+              {list.map((i, index) => (
+                <div
+                className='option-suggest'
+                key={index}
+                onClick={() => {
+                  this.setState({
+                    setValue: i,
+                    showDatalist: false
+                  })
+                  onSelect(i)
+                }}>{i}</div>
+              ))}
+            </div>
+          }
+        </div>
       </div>
     )
   }
